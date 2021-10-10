a054fe11210ce7c54c4cf658d8493cab045f04bb
senthilsweb <senthilsweb@gmail.com>
2021-10-10 17:40:09 -0400

    Introduced mailgun template

--- controller/mailgunController.go
+++ controller/mailgunController.go
@@ -4,51 +4,55 @@ import (
 	"bytes"
 	"context"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 
-	"github.com/nikoksr/notify"
-	"github.com/nikoksr/notify/service/mailgun"
+	mailgun "github.com/mailgun/mailgun-go/v4"
 	"github.com/tidwall/gjson"
 )
 
 func NotifyMailgun(c *gin.Context) {
 
-	notifier := notify.New()
-
-	// Provide your mailgun details
-	//os.Getenv("")
-	mailgunService := mailgun.New(os.Getenv("MAILGUN_DOMAIN"), os.Getenv("MAILGUN_KEY"), os.Getenv("EMAIL_SENDER"))
-	mailgunService.AddReceivers(os.Getenv("EMAIL_TEST_RECEIVER"))
-	notifier.UseServices(mailgunService)
-
 	jsonData, err := c.GetRawData()
 	input := bytes.NewBuffer(jsonData).String()
-
-	if err != nil {
-		log.Fatal(err)
-		c.JSON(500, gin.H{"success": "false", "message": err})
-		return
-	}
-
 	subject := gjson.Get(input, "message.subject")
 	body := gjson.Get(input, "message.body")
+	log.Info("MAILGUN_DOMAIN ============================ " + os.Getenv("MAILGUN_DOMAIN"))
 	log.Info("Subject = " + subject.String())
 	log.Info("Body = " + body.String())
-	// Send a message
-	err = notifier.Send(
-		context.Background(),
-		subject.String(),
-		body.String(),
-	)
 
+	// Create an instance of the Mailgun Client
+	mg := mailgun.NewMailgun(os.Getenv("MAILGUN_DOMAIN"), os.Getenv("MAILGUN_KEY"))
+
+	sender := os.Getenv("EMAIL_SENDER")
+
+	recipient := os.Getenv("EMAIL_TEST_RECEIVER")
+
+	// The message object allows you to add attachments and Bcc recipients
+	message := mg.NewMessage(sender, subject.String(), "", recipient)
+	message.SetTemplate("welcome_email")
+	err = message.AddTemplateVariable("passwordResetLink", "some link to your site unique to your user")
 	if err != nil {
 		log.Fatal(err)
 		c.JSON(500, gin.H{"success": "false", "message": err})
 		return
 	}
 
-	c.JSON(200, gin.H{"success": "true", "message": "Email has been sent successfully"})
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	// Send the message with a 10 second timeout
+	resp, id, err := mg.Send(ctx, message)
+
+	if err != nil {
+		log.Fatal(err)
+		c.JSON(500, gin.H{"success": "false", "message": err})
+		return
+	}
+	log.Info(id)
+	c.JSON(200, gin.H{"success": "true", "message": "Email has been sent successfully", "m": resp})
 	return
+
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/aymerick/raymond v2.0.2+incompatible
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.7.4
+	github.com/mailgun/mailgun-go/v4 v4.5.2
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/nikoksr/notify v0.17.3
 	github.com/sirupsen/logrus v1.8.1
