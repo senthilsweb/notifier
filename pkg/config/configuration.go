31e4258e90b6fc2fa137da993e0782721686bdca
senthilsweb <senthilsweb@gmail.com>
2021-10-10 12:38:46 -0400

    fix for config file path

--- pkg/config/configuration.go
+++ pkg/config/configuration.go
@@ -14,6 +14,7 @@ func Setup() {
 	viper.AddConfigPath(".")
 	viper.AddConfigPath("./bin")
 	viper.AddConfigPath("./config")
+	viper.AddConfigPath("./functions")
 	viper.AllowEmptyEnv(true)
 
 	if err := viper.ReadInConfig(); err != nil {
